8e8c5fced1c4427e02edbad71806250dface55fe
Noa Barzelay <t-nobarz@microsoft.com>
2021-08-29 22:59:38 +0300

    deployment

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -8,7 +8,6 @@ contract DEX {
     event Bought(uint256 amount);
     event Sold(uint256 amount);
 
-
     ERC20Interface public token;
     string public name = 'DEX';
 
@@ -23,20 +22,14 @@ contract DEX {
         require(amountTobuy > 0, "You need to send some ether");
         require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
         token.transfer(msg.sender, amountTobuy); 
-        // token.transferFrom(address(this), msg.sender, amountTobuy);
         emit Bought(amountTobuy);
     }
 
     function sellTokens(uint256 amount) public {
         require(amount > 0, "You need to sell at least some tokens");
         require(address(this).balance >= amount, "Not enough ether in the reserve");
-        uint256 allowance = token.allowance(msg.sender, address(this));
-      //  require(allowance >= amount, "Check the token allowance");
         token.transferFrom(msg.sender, address(this), amount);
         payable(msg.sender).transfer(amount);
         emit Sold(amount);
     } 
-
-   /* function () external payable {   
-    } */
 } 
